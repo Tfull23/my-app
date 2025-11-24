98788266bd84a13f1a3f507b838f02416ed802a9
Tarence Fuller <tarencefuller@icloud.com>
2025-11-23 18:59:22 -0800

    Project and Service component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeHomeComponent } from './home-home/home-home.component';
 import { ContactHomeComponent } from './contact-home/contact-home.component';
-
+import { ProjectComponent } from './project/project.component';
+import { ServiceComponent } from './service/service.component';
 export const routes: Routes = [
   {path: 'home', component: HomeHomeComponent}
   {path: 'contact', component: ContactHomeComponent}
+  {path: 'project', component: ProjectComponent}
+  {path: 'service', component: ServiceComponent}
 ];
