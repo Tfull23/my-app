f63b49ab6375034c88b22e7024ab7784c3b11c54
Tarence Fuller <tarencefuller@icloud.com>
2025-11-23 19:09:50 -0800

    components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { HomeHomeComponent } from './home-home/home-home.component';
 import { ContactHomeComponent } from './contact-home/contact-home.component';
 import { ProjectComponent } from './project/project.component';
 import { ServiceComponent } from './service/service.component';
+
 export const routes: Routes = [
-  {path: 'home', component: HomeHomeComponent}
-  {path: 'contact', component: ContactHomeComponent}
-  {path: 'project', component: ProjectComponent}
-  {path: 'service', component: ServiceComponent}
+  {path: 'home', component: HomeHomeComponent},
+  {path: 'contact', component: ContactHomeComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'service', component: ServiceComponent},
 ];
