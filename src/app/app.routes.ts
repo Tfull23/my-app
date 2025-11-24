97c943f4dfcc438e680e7faf80c2305a5f001641
Tarence Fuller <tarencefuller@icloud.com>
2025-11-23 18:50:28 -0800

    ContactHome created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeHomeComponent } from './home-home/home-home.component';
+import { ContactHomeComponent } from './contact-home/contact-home.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeHomeComponent}
+  {path: 'contact', component: ContactHomeComponent}
 ];
