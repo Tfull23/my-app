920317434db044267b46cad469938c039fcac934
tarence Fuller <tarencefuller@icloud.com>
2025-12-29 18:09:50 -0800

    add code to app.component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WikipediaService } from './wikipedia.service';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SearchBarComponent, PageListComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'TechX Consulting Services';
+  title = 'Wikipedia Search';
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
+  }
 }
